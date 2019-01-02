a916df68d30315041ad353be22c96d3ca27c3901
David <davidbrio@hotmail.com>
2019-01-02 17:09:07 +0100

    refact use case

--- domain/src/main/java/com/example/chin/domain/main/MainUseCaseImpl.kt
+++ domain/src/main/java/com/example/chin/domain/main/MainUseCaseImpl.kt
@@ -1,13 +1,15 @@
 package com.example.chin.domain.main
 
 import com.example.chin.data.gateways.MainLocalGateway
-import com.example.chin.domain.BaseUseCase
 import com.example.chin.domain.entities.ShoppingItem
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import javax.inject.Inject
 
-class MainUseCaseImpl @Inject constructor(job: Job, val mainLocalGateway: MainLocalGateway): BaseUseCase<String, List<ShoppingItem>>(job), MainUseCase {
+class MainUseCaseImpl @Inject constructor(
+    override val job: Job,
+    private val mainLocalGateway: MainLocalGateway
+): MainUseCase {
 
     override suspend fun run(input: String): List<ShoppingItem> {
 
