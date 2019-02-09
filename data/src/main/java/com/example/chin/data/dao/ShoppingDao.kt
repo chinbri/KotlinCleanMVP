72ac49b8437c3ff0b0f77be078473684100e0aed
David <davidbrio@hotmail.com>
2019-02-09 19:36:52 +0100

    gradle update. Room library update.

--- data/src/main/java/com/example/chin/data/dao/ShoppingDao.kt
+++ data/src/main/java/com/example/chin/data/dao/ShoppingDao.kt
@@ -16,24 +16,24 @@ interface ShoppingDao {
     }
 
     @Query("SELECT * FROM $TABLE_NAME")
-    fun getAll(): List<ShoppingLocalEntity>
+    suspend fun getAll(): List<ShoppingLocalEntity>
 
     @Query("SELECT * FROM $TABLE_NAME WHERE $COLUMN_NAME IN (:userNames)")
-    fun loadAllByIds(vararg userNames: String): List<ShoppingLocalEntity>
+    suspend fun loadAllByIds(vararg userNames: String): List<ShoppingLocalEntity>
 
     @Query("SELECT * FROM $TABLE_NAME WHERE $COLUMN_NAME LIKE :name LIMIT 1")
-    fun findByName(name: String): ShoppingLocalEntity?
+    suspend fun findByName(name: String): ShoppingLocalEntity?
 
     @Query("SELECT * FROM $TABLE_NAME WHERE $COLUMN_ID LIKE :id LIMIT 1")
-    fun findById(id: Int): ShoppingLocalEntity?
+    suspend fun findById(id: Int): ShoppingLocalEntity?
 
     @Insert
-    fun insertAll(vararg users: ShoppingLocalEntity)
+    suspend fun insertAll(vararg users: ShoppingLocalEntity)
 
     @Delete
-    fun delete(item: ShoppingLocalEntity)
+    suspend fun delete(item: ShoppingLocalEntity)
 
     @Update
-    fun update(item: ShoppingLocalEntity)
+    suspend fun update(item: ShoppingLocalEntity)
 
 }
